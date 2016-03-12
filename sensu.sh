c1596e4faee85e3a983d2ae1d9d9a9a8bffc65cc
Chaitanya Akkineni <cakkineni@gmail.com>
2016-03-13 01:28:11 +0530

    Adding sensu server

---
+++ sensu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+RUNDIR=/var/run/sensu-server
+PIDFILE=$RUNDIR/sensu-server.pid
+
+mkdir -p $RUNDIR
+touch $PIDFILE
+chown sensu:sensu $RUNDIR $PIDFILE
+chmod 755 $RUNDIR
+
+
+exec /opt/sensu/bin/sensu-server -d /etc/sensu -p $PIDFILE &
+sleep 5
+
+exec /opt/sensu/bin/sensu-api -d /etc/sensu
